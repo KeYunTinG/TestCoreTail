48867ee5d9c2367f20e39b192511e720f498fee5
KeYunTinG <zaac2240632@yahoo.com.tw>
2024-05-08 00:35:59 +0800

    orderList1

--- src/views/admin/OrderList.tsx
+++ src/views/admin/OrderList.tsx
@@ -1,5 +1,27 @@
 import '../../../scss/tailwind.scss'
 import { useParams } from 'react-router-dom';
+import {
+  cibCcAmex,
+  cibCcApplePay,
+  cibCcMastercard,
+  cibCcPaypal,
+  cibCcStripe,
+  cibCcVisa,
+  cibGoogle,
+  cibFacebook,
+  cibLinkedin,
+  cifBr,
+  cifEs,
+  cifFr,
+  cifIn,
+  cifPl,
+  cifUs,
+  cibTwitter,
+  cilCloudDownload,
+  cilPeople,
+  cilUser,
+  cilUserFemale,
+} from '@coreui/icons'
 const orderList = () => {
     let { id } = useParams();
   
@@ -58,34 +80,56 @@ const people = [
       lastSeen: null,
     },
   ]
+  const ordersTable = [
+    {
+      project:
+        '【PHILIPS 黑金剛】三合一磁吸充電系列｜一秒強勁吸附，智慧溫控晶片，簡化生活快速時尚！',
+      productOrders: [
+        {
+          name: '早鳥 旗艦組',
+          price: 2990,
+          count: 2,
+        },
+        {
+          name: '早鳥 三合一充電組',
+          price: 2390,
+          count: 3,
+        },
+        {
+          name: '早鳥 行動電源組',
+          price: 1790,
+          count: 4,
+        },
+        {
+          name: '超早鳥 旗艦組',
+          price: 2850,
+          count: 7,
+        },
+      ],
+      price: 71711,
+      payment: { name: 'PayPal', icon: cibCcPaypal },
+      purchaseTime: '2024-04-11',
+    },
+  ]
   
     return (
         <>
         <h1>{id}</h1>
       <ul role="list" className="divide-y divide-gray-100">
         {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4">
+          <li key={person.email} className="flex justify-between gap-x-6 py-3 hover:bg-gray-200">
+            <div className="flex min-w-0 gap-x-3 pl-2">
               <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                <p className="text-lg font-semibold leading-6 text-gray-900">{person.name}</p>
+                <p className="mt-1 truncate text-base leading-5 text-gray-500">{person.email}</p>
               </div>
             </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-              {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end pr-2">
+              <p className="text-base leading-6 text-gray-900">{person.role}</p>
+                <p className="mt-1 text-base leading-5 text-gray-500">
                   Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
                 </p>
-              ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                  </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
-                </div>
-              )}
             </div>
           </li>
         ))}
